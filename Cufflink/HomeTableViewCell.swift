8635a2980c2fd3a7423243863e6b3fa3179d870c
Zuri Wong <zuriwong@Zuris-MacBook-Pro.local>
2018-04-10 11:58:37 -0400

    homeTableViewController not working properly

---
+++ Cufflink/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Cufflink
+//
+//  Created by Zuri Wong on 4/9/18.
+//  Copyright © 2018 Zuri Wong. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
